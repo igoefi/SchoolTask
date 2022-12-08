76fc5d9280a1251a219ea6156fc8cafff5fb8689
igoefi <igoefi@mail.ru>
2022-12-08 10:11:42 +0300

    Initial commit

---
+++ ConsoleApp11/Classes/MainClasses.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Classes
+{
+    #region People
+    [Serializable()]
+    internal abstract class Human 
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        internal Human(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+
+    [Serializable()]
+    internal class Student : Human, IComparable<Student> 
+    {
+        private Dictionary<string, List<float>> _lessons = new Dictionary<string, List<float>>();
+        internal Student(string name, string surname) : base(name, surname) { }
+
+        public void AddOrRateLesson(string lessonName, float mark)
+        {
+            if (_lessons[lessonName] == null)
+                _lessons.Add(lessonName, new List<float>());
+
+            float realMark = mark > 2 ? mark < 5 ? mark : 5 : 2;
+            _lessons[lessonName].Add(realMark);
+        }
+
+        public void DeleteLesson(string name) => _lessons.Remove(name);
+        public List<float> GetMarks(string lesson)
+        {
+            return _lessons[lesson];
+        }
+
+        public int CompareTo(Student other)
+        {
+            string fullName = Surname + Name;
+            return fullName.CompareTo(other.Surname + other.Name);
+        }
+    }
+
+    [Serializable()]
+    internal class Teacher : Human
+    {
+        private Dictionary<string, List<Class>> _lessons = new Dictionary<string, List<Class>>();
+        public uint _mainClassNum { get; private set; }
+        internal Teacher(string name, string surname, uint mainClassNum) : base(name, surname) 
+            => _mainClassNum= mainClassNum;
+
+        public void AddClassOrLesson(string lesson, Class lessonClass)
+        {
+            if (lessonClass == null) return;
+
+            if (_lessons[lesson] == null)
+                _lessons.Add(lesson, new List<Class>());
+
+            _lessons[lesson].Add(lessonClass);
+        }
+    }
+    #endregion
+
+    [Serializable()]
+    internal class Class : IComparable<Class> 
+    {
+        public string Name { get; private set; }
+
+        public List<Student> Students { get; private set; } = new List<Student>();
+        private Dictionary<string, Teacher> _lessonsTeachers = new Dictionary<string, Teacher>();
+
+        internal Class(string name)
+        {
+            Name = name;
+        }
+
+        public void AddStudent(Student student) => Students.Add(student);
+        public void DeleteStudent(Student student) => Students.Remove(student);
+
+        public void AddLesson(string nameLesson, Teacher teacher) =>
+            _lessonsTeachers.Add(nameLesson, teacher);
+        public void DeleteLession(string nameLesson) =>
+            _lessonsTeachers.Remove(nameLesson);
+
+        public float GetAverageRating(string lessonName)
+        {
+            float rating = 0;
+            foreach(Student student in Students)
+            {
+                rating += student.GetMarks(lessonName).Last();
+            }
+            return rating / Students.Count;
+        }
+
+        public int CompareTo(Class other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using School.Classes;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace School
+{
+    [Serializable()]
+    internal class Program
+    {
+        const string FileName = @"../../../SavedClass.json";
+        private static Arrays _arrays;
+
+        static void Main(string[] args)
+        {
+            _arrays = new Arrays();
+            if (File.Exists(FileName))
+            {
+                _arrays = ReadFile(out Stream stream);
+                stream.Close();
+            }
+
+
+          
+            Console.WriteLine("Вывод всех учеников из всех классов");
+            foreach(Class schClass in _arrays._classes)
+            {
+                Console.WriteLine($"Class {schClass.Name}");
+                WriteStudentsOfClass(schClass.Name);
+            }
+
+            SaveFile();
+        }
+
+        #region AddDeleteAnything
+        private static void AddDeleteClass(bool isAdd)
+        {
+            Console.WriteLine("Введите название класса");
+            string className = Console.ReadLine();
+            _arrays.AddDeleteClass(className, isAdd);
+        }
+
+        private static void AddDeleteStudent(bool isAdd)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Введите название класса, куда необходимо добавить ученика: ");
+            string className = Console.ReadLine();
+            if(_arrays.FindClass(className) == null)
+            {
+                Console.WriteLine("Такого класса не существует");
+                return;
+            }
+
+            Console.WriteLine("Введите имя и фамилию ученика этого класса");
+            string studentName = Console.ReadLine();
+            string studentSurname = Console.ReadLine();
+
+           _arrays.AddDeleteStudent(className, studentName, studentSurname, isAdd);
+        }
+
+        private static void AddDeleteTeacher(string name, string surname, uint mainClassNum, bool isAdd)
+        {
+            Console.Clear();
+            Console.WriteLine("Добавление учителя");
+
+            Console.WriteLine("Напишите имя и фамилию учителя");
+        }
+        #endregion
+
+        private static void WriteStudentsOfClass(string className)
+        {
+            Class schClass = _arrays.FindClass(className);
+            if (schClass != null)
+                foreach (Student student in schClass.Students)
+                    Console.WriteLine($"Ученик {student.Surname} {student.Name}");
+        }
+
+        #region Serialization
+        private static Arrays ReadFile(out Stream stream)
+        {
+            Console.WriteLine("Reading saved file");
+            stream = File.OpenRead(FileName);
+            BinaryFormatter deserializer = new BinaryFormatter();
+            return (Arrays)deserializer.Deserialize(stream);
+        }
+
+        private static void SaveFile()
+        {
+            Stream SaveFileStream = File.Create(FileName);
+            BinaryFormatter serializer = new BinaryFormatter();
+            serializer.Serialize(SaveFileStream, _arrays);
+            SaveFileStream.Close();
+        }
+        #endregion
+    }
+
+    [Serializable()]
+    internal class Arrays
+    {
+        public List<Class> _classes = new List<Class>();
+        public List<Teacher> _teachers = new List<Teacher>();
+
+        #region Add&DeleteSomeone
+        public void AddDeleteClass(string name, bool isAdd)
+        {
+            if (FindClass(name) != null)
+            {
+                if (!isAdd)
+                    _classes.Remove(FindClass(name));
+                else
+                    Console.WriteLine("This class is here");
+
+                return;
+            }
+
+            _classes.Add(new Class(name));
+            _classes.Sort();
+        }
+
+        public void AddDeleteStudent(string className, string stundentName, string studentSurname, bool isAdd)
+        {
+            Class schClass = FindClass(className);
+            if (schClass == null)
+            {
+                Console.WriteLine("Класс не найден");
+                return;
+            }
+
+            if (isAdd)
+            {
+                schClass.AddStudent(new Student(stundentName, studentSurname));
+                schClass.Students.Sort();
+            }
+            else
+            {
+                schClass.DeleteStudent(FindStudent(schClass, stundentName, studentSurname));
+            }
+        }
+
+        public void AddDeleteLesson(string className, string lesson, string nameTeacher, string surnameTeacher, bool isAdd)
+        {
+            Class schClass = FindClass(className);
+            Teacher teacher = FindTeacher(nameTeacher, surnameTeacher);
+            if (teacher == null)
+            {
+                Console.WriteLine($"Учитель {surnameTeacher} {nameTeacher} нет в базе данных");
+                return;
+            }
+            if (schClass == null)
+            {
+                Console.WriteLine($"Класса под номером {className} не существует. Вы можете создать его");
+                return;
+            }
+
+            if (isAdd)
+                schClass.AddLesson(lesson, teacher);
+            else
+                schClass.DeleteLession(lesson);
+
+        }
+
+        public void AddDeleteTeacher(string name, string surname, uint mainClassNum, bool isAdd)
+        {
+            Teacher teacher = FindTeacher(name, surname);
+            if (teacher != null)
+            {
+                if (!isAdd)
+                    _teachers.Remove(FindTeacher(name, surname));
+                else
+                    Console.WriteLine("Этот учитель уже записан в базе данных");
+
+                return;
+            }
+
+            _teachers.Add(new Teacher(name, surname, mainClassNum));
+        }
+        #endregion
+
+        #region FindInBase
+        public Class FindClass(string name)
+        {
+            foreach (Class schooleClass in _classes)
+                if (schooleClass.Name == name) return schooleClass;
+            return null;
+        }
+
+        public Teacher FindTeacher(string name, string surname)
+        {
+            foreach (Teacher teacher in _teachers)
+                if (teacher.Name == name && teacher.Surname == surname) return teacher;
+            return null;
+        }
+
+        private Student FindStudent(Class schClass, string name, string surname)
+        {
+            foreach (Student student in schClass.Students)
+                if (student.Name == name && student.Surname == surname) return student;
+            return null;
+        }
+        #endregion
+    }
+}
