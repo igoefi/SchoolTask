4f96c5c1863816dbda337fc24950e93a8e227a13
igoefi <igoefi@mail.ru>
2022-12-15 23:23:10 +0300

    update

---
+++ ConsoleApp11/Classes/Arrays.cs
@@ -0,0 +1,334 @@
+﻿using School.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11.Classes
+{
+    [Serializable()]
+    internal class Arrays
+    {
+        public List<Class> _classes = new List<Class>();
+        public List<Teacher> _teachers = new List<Teacher>();
+
+        #region Add&DeleteSomeone
+        public void AddDeleteClass(string name, bool isAdd)
+        {
+            if (FindClass(name) != null)
+            {
+                if (!isAdd)
+                    _classes.Remove(FindClass(name));
+                else
+                    Console.WriteLine("Этот класс уже существует");
+
+                return;
+            }
+
+            if (isAdd)
+                _classes.Add(new Class(name));
+            else
+                Console.WriteLine("Этого класса не существует");
+            _classes.Sort();
+        }
+
+        public void AddDeleteStudent(string className, string studentName, string studentSurname, bool isAdd)
+        {
+            Class schClass = FindClass(className);
+            if (schClass == null)
+            {
+                Console.WriteLine("Класс не найден");
+                return;
+            }
+            if (FindStudent(schClass, studentName, studentSurname) != null && isAdd)
+            {
+                Console.WriteLine($"Студент {studentSurname} {studentName} уже находится в этом классе");
+                return;
+            }
+
+            if (isAdd)
+            {
+                schClass.AddStudent(new Student(studentName, studentSurname));
+            }
+            else
+            {
+                schClass.DeleteStudent(FindStudent(schClass, studentName, studentSurname));
+            }
+            schClass.Students.Sort();
+        }
+
+        public void AddDeleteLesson(string className, string lesson, string nameTeacher, string surnameTeacher, bool isAdd)
+        {
+            Class schClass = FindClass(className);
+            Teacher teacher = FindTeacher(nameTeacher, surnameTeacher);
+            if (teacher == null)
+            {
+                Console.WriteLine($"Учителя {surnameTeacher} {nameTeacher} нет в базе данных");
+                return;
+            }
+            if (schClass == null)
+            {
+                Console.WriteLine($"Класса под номером {className} не существует. Вы можете создать его");
+                return;
+            }
+
+            schClass.AddDeleteLesson(lesson, teacher, isAdd);
+            if (isAdd)
+                teacher.AddClassOrLesson(lesson, schClass);
+            else
+                teacher.DeleteLesson(lesson, schClass);
+        }
+        public void AddDeleteLesson(string lesson, string nameTeacher, string surnameTeacher, bool isAdd)
+        {
+            Teacher teacher = FindTeacher(nameTeacher, surnameTeacher);
+            if (teacher == null)
+            {
+                Console.WriteLine($"Учителя {surnameTeacher} {nameTeacher} нет в базе данных");
+                return;
+            }
+            foreach (Class schClass in _classes)
+                schClass.AddDeleteLesson(lesson, teacher, isAdd);
+
+            if (isAdd)
+            {
+                foreach (Class schClass in _classes)
+                    teacher.AddClassOrLesson(lesson, schClass);
+            }
+            else
+            {
+                teacher.DeleteLesson(lesson);
+            }
+        }
+
+        public void AddDeleteTeacher(string name, string surname, uint mainClassNum, bool isAdd)
+        {
+            Teacher teacher = FindTeacher(name, surname);
+            if (teacher != null)
+            {
+                if (!isAdd)
+                    _teachers.Remove(FindTeacher(name, surname));
+                else
+                    Console.WriteLine("Этот учитель уже записан в базе данных");
+
+                return;
+            }
+
+            if (isAdd)
+                _teachers.Add(new Teacher(name, surname, mainClassNum));
+            else
+                Console.WriteLine("Этого учителя нет");
+        }
+        #endregion
+        #region GetSetSomeone
+        #region SetMark
+        public void SetMark(string className, string studentName, string studentSurname, string lessonName, float mark, uint quarter)
+        {
+            Class schClass = FindClass(className);
+            if (schClass == null)
+            {
+                Console.WriteLine($"Класса с номером {className} не существует");
+                return;
+            }
+
+            Student student = FindStudent(schClass, studentName, studentSurname);
+            if (student == null)
+            {
+                Console.WriteLine($"Студента {studentSurname} {studentName} не существует");
+                return;
+            }
+
+            float realMark = mark > 2 ? mark < 5 ? mark : 5 : 2;
+            student.LessonsMark.TryGetValue(lessonName, out List<float> lessonRate);
+            if (lessonRate == null)
+            {
+                Console.WriteLine("Такого пердмета не существует в этом классе");
+                return;
+            }
+            if (lessonRate.Count < quarter - 1)
+            {
+                Console.WriteLine("Сначала заполните предыдущие четверти");
+                return;
+            }
+            if (quarter < 1)
+                return;
+            lessonRate[(int)quarter - 1] = realMark;
+        }
+        public void SetMark(string className, string studentName, string studentSurname, string lessonName, float mark)
+        {
+            Class schClass = FindClass(className);
+            if (schClass == null)
+            {
+                Console.WriteLine($"Класса с номером {className} не существует");
+                return;
+            }
+
+            Student student = FindStudent(schClass, studentName, studentSurname);
+            if (student == null)
+            {
+                Console.WriteLine($"Студента {studentSurname} {studentName} не существует");
+                return;
+            }
+
+            float realMark = mark > 2 ? mark < 5 ? mark : 5 : 2;
+            student.LessonsMark.TryGetValue(lessonName, out List<float> marks);
+            if (marks == null)
+            {
+                Console.WriteLine($"Предмет {lessonName} в классе {className} не преподаётся");
+            }
+            else
+            {
+                marks.Add(realMark);
+            }
+        }
+        #endregion
+        #region GetRating
+        public void GetClassRating(out List<Class> classes, out List<float> marks)
+        {
+            classes = new List<Class>();
+            marks = new List<float>();
+            if (_classes.Count == 0) return;
+            foreach (Class schClass in _classes)
+            {
+                if (schClass.GetLessons().Count == 0) continue;
+                if (schClass.Students.Count == 0) continue;
+
+                classes.Add(schClass);
+                var averangeMarks = schClass.GetAverageRatingOfClass();
+                float sumMarks = 0;
+                float lessonsCount = 0;
+                foreach (string lesson in averangeMarks.Keys)
+                {
+                    List<float> lessonsMark = averangeMarks[lesson];
+                    if (lessonsMark.Count == 0) continue;
+
+                    lessonsCount++;
+                    sumMarks += lessonsMark.Sum() / lessonsMark.Count();
+                }
+                marks.Add(lessonsCount > 0 ? sumMarks / lessonsCount : 0);
+            }
+
+            //Не бейте за пузырька
+            Class mejClass;
+            float mejMark;
+            for (int i = 0; i < marks.Count; i++)
+            {
+                for (int j = i + 1; j < marks.Count; j++)
+                {
+                    if (marks[i] < marks[j])
+                    {
+                        mejClass = classes[i];
+                        mejMark = marks[i];
+
+                        marks[i] = marks[j];
+                        marks[j] = mejMark;
+                        classes[i] = classes[j];
+                        classes[j] = mejClass;
+                    }
+                }
+            }
+        }
+        public void GetTeacherRating(out List<Teacher> teachers, out List<float> marks)
+        {
+            teachers = new List<Teacher>();
+            marks = new List<float>();
+
+            if (_teachers.Count == 0) return;
+
+            foreach (Teacher teacher in _teachers)
+            {
+                teachers.Add(teacher);
+
+                var teacherMarks = teacher.GetAllMarks();
+
+                if (teacherMarks == null)
+                {
+                    marks.Add(0);
+                    return;
+                }
+                //Почему я просто не сделал "marks.Add(teacherMarks.Values.Sum() / teacherMarks.Count())" ? Потому что это не учитывает моменты,
+                //когда оценки по предметам не расставлдены, и получается, что число в сумму не прибавляется(+0), но делится на большее кол-во уроков
+                float sumMarks = 0;
+                int sumLessons = 0;
+                foreach (string lesson in teacherMarks.Keys)
+                {
+                    if (teacherMarks[lesson] == 0) continue;
+
+                    sumMarks += teacherMarks[lesson];
+                    sumLessons++;
+                }
+                marks.Add(sumLessons > 0 ? sumMarks / sumLessons : 0);
+            }
+
+            //Не бейте за пузырька во второй раз
+            Teacher mejClass;
+            float mejMark;
+            for (int i = 0; i < marks.Count; i++)
+            {
+                for (int j = i + 1; j < marks.Count; j++)
+                {
+                    if (marks[i] < marks[j])
+                    {
+                        mejClass = teachers[i];
+                        mejMark = marks[i];
+
+                        marks[i] = marks[j];
+                        marks[j] = mejMark;
+                        teachers[i] = teachers[j];
+                        teachers[j] = mejClass;
+                    }
+                }
+            }
+
+        }
+        #endregion
+
+        public List<float> GetMarks(string className, string studentName, string studentSurname, string lessonName)
+        {
+            Class schClass = FindClass(className);
+            if (schClass == null)
+            {
+                Console.WriteLine($"Класса с номером {className} не существует");
+                return null;
+            }
+            Student student = FindStudent(schClass, studentName, studentSurname);
+            if (student == null)
+            {
+                Console.WriteLine($"Студента {studentSurname} {studentName} не существует");
+                return null;
+            }
+            return student.LessonsMark[lessonName];
+        }
+
+        public List<string> GetLessons(string className)
+        {
+            List<string> lessons = FindClass(className).GetLessons();
+            return lessons;
+        }
+        #endregion
+        #region FindInBase
+        public Class FindClass(string name)
+        {
+            foreach (Class schooleClass in _classes)
+                if (schooleClass.Name.ToLower() == name.ToLower()) return schooleClass;
+            return null;
+        }
+
+        public Teacher FindTeacher(string name, string surname)
+        {
+            if (_teachers.Count > 0)
+                foreach (Teacher teacher in _teachers)
+                    if (teacher.Name.ToLower() == name.ToLower() && teacher.Surname.ToLower() == surname.ToLower()) return teacher;
+            return null;
+        }
+
+        public Student FindStudent(Class schClass, string name, string surname)
+        {
+            foreach (Student student in schClass.Students)
+                if (student.Name.ToLower() == name.ToLower() && student.Surname.ToLower() == surname.ToLower()) return student;
+            return null;
+        }
+        #endregion
+    }
+}
+
--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,4 +1,5 @@
-﻿using School.Classes;
+﻿using ConsoleApp11.Classes;
+using School.Classes;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -64,6 +65,7 @@ namespace School
             Console.WriteLine("4 - Удалить предмет, который ведёт учитель");
             Console.WriteLine("5 - Вывеси список всех учителей");
             Console.WriteLine("6 - Вывести информацию о конкретном учителе");
+            Console.WriteLine("7 - Вывести рейтинг учителей");
             ConsoleKeyInfo key = Console.ReadKey();
             Console.Clear();
             switch (key.KeyChar)
@@ -82,9 +84,12 @@ namespace School
                     break;
                 case '5':
                     WriteAllTeachers();
-                    break;      
+                    break;
                 case '6':
                     FindAndWriteInfoAboutTeacher();
+                    break;
+                case '7':
+                    WriteTeacherRating();
                     break;
                 default:
                     Console.Clear();
@@ -268,16 +273,21 @@ namespace School
         private static void WriteClassRating()
         {
             _arrays.GetClassRating(out List<Class> classes, out List<float> marks);
-            for(int classNum = 0; classNum < classes.Count; classNum++)
+            for (int classNum = 0; classNum < classes.Count; classNum++)
             {
-                Console.WriteLine($"{classNum+1} класс {classes[classNum].Name}." + (marks[classNum] > 0 ?
-                    $" Средняя оценка - {marks[classNum]}" : " Оценка не выставлена") );
+                Console.WriteLine($"{classNum + 1} класс {classes[classNum].Name}." + (marks[classNum] > 0 ?
+                    $" Средняя оценка - {marks[classNum]}" : " Оценка не выставлена"));
             }
         }
 
         private static void WriteTeacherRating()
         {
-
+            _arrays.GetTeacherRating(out List<Teacher> teachers, out List<float> marks);
+            for (int teacherNum = 0; teacherNum < teachers.Count; teacherNum++)
+            {
+                Console.WriteLine($"{teacherNum + 1} класс {teachers[teacherNum].Name}." + (marks[teacherNum] > 0 ?
+                    $" Средняя оценка - {marks[teacherNum]}" : " Оценки не выставлены"));
+            }
         }
         #endregion
 
@@ -504,274 +514,4 @@ namespace School
         #endregion
     }
 
-    [Serializable()]
-    internal class Arrays
-    {
-        public List<Class> _classes = new List<Class>();
-        public List<Teacher> _teachers = new List<Teacher>();
-
-        #region Add&DeleteSomeone
-        public void AddDeleteClass(string name, bool isAdd)
-        {
-            if (FindClass(name) != null)
-            {
-                if (!isAdd)
-                    _classes.Remove(FindClass(name));
-                else
-                    Console.WriteLine("Этот класс уже существует");
-
-                return;
-            }
-
-            if (isAdd)
-                _classes.Add(new Class(name));
-            else
-                Console.WriteLine("Этого класса не существует");
-            _classes.Sort();
-        }
-
-        public void AddDeleteStudent(string className, string studentName, string studentSurname, bool isAdd)
-        {
-            Class schClass = FindClass(className);
-            if (schClass == null)
-            {
-                Console.WriteLine("Класс не найден");
-                return;
-            }
-            if (FindStudent(schClass, studentName, studentSurname) != null && isAdd)
-            {
-                Console.WriteLine($"Студент {studentSurname} {studentName} уже находится в этом классе");
-                return;
-            }
-
-            if (isAdd)
-            {
-                schClass.AddStudent(new Student(studentName, studentSurname));
-            }
-            else
-            {
-                schClass.DeleteStudent(FindStudent(schClass, studentName, studentSurname));
-            }
-            schClass.Students.Sort();
-        }
-
-        public void AddDeleteLesson(string className, string lesson, string nameTeacher, string surnameTeacher, bool isAdd)
-        {
-            Class schClass = FindClass(className);
-            Teacher teacher = FindTeacher(nameTeacher, surnameTeacher);
-            if (teacher == null)
-            {
-                Console.WriteLine($"Учителя {surnameTeacher} {nameTeacher} нет в базе данных");
-                return;
-            }
-            if (schClass == null)
-            {
-                Console.WriteLine($"Класса под номером {className} не существует. Вы можете создать его");
-                return;
-            }
-
-            schClass.AddDeleteLesson(lesson, teacher, isAdd);
-            if (isAdd)
-                teacher.AddClassOrLesson(lesson, schClass);
-            else
-                teacher.DeleteLesson(lesson, schClass);
-        }
-        public void AddDeleteLesson(string lesson, string nameTeacher, string surnameTeacher, bool isAdd)
-        {
-            Teacher teacher = FindTeacher(nameTeacher, surnameTeacher);
-            if (teacher == null)
-            {
-                Console.WriteLine($"Учителя {surnameTeacher} {nameTeacher} нет в базе данных");
-                return;
-            }
-            foreach (Class schClass in _classes)
-                schClass.AddDeleteLesson(lesson, teacher, isAdd);
-
-            if (isAdd)
-            {
-                foreach (Class schClass in _classes)
-                    teacher.AddClassOrLesson(lesson, schClass);
-            }
-            else
-            {
-                teacher.DeleteLesson(lesson);
-            }
-        }
-
-        public void AddDeleteTeacher(string name, string surname, uint mainClassNum, bool isAdd)
-        {
-            Teacher teacher = FindTeacher(name, surname);
-            if (teacher != null)
-            {
-                if (!isAdd)
-                    _teachers.Remove(FindTeacher(name, surname));
-                else
-                    Console.WriteLine("Этот учитель уже записан в базе данных");
-
-                return;
-            }
-
-            if (isAdd)
-                _teachers.Add(new Teacher(name, surname, mainClassNum));
-            else
-                Console.WriteLine("Этого учителя нет");
-        }
-        #endregion
-        #region GetSetSomeone
-        #region SetMark
-        public void SetMark(string className, string studentName, string studentSurname, string lessonName, float mark, uint quarter)
-        {
-            Class schClass = FindClass(className);
-            if (schClass == null)
-            {
-                Console.WriteLine($"Класса с номером {className} не существует");
-                return;
-            }
-
-            Student student = FindStudent(schClass, studentName, studentSurname);
-            if (student == null)
-            {
-                Console.WriteLine($"Студента {studentSurname} {studentName} не существует");
-                return;
-            }
-
-            float realMark = mark > 2 ? mark < 5 ? mark : 5 : 2;
-            student.LessonsMark.TryGetValue(lessonName, out List<float> lessonRate);
-            if (lessonRate == null)
-            {
-                Console.WriteLine("Такого пердмета не существует в этом классе");
-                return;
-            }
-            if (lessonRate.Count < quarter - 1)
-            {
-                Console.WriteLine("Сначала заполните предыдущие четверти");
-                return;
-            }
-            if (quarter < 1)
-                return;
-            lessonRate[(int)quarter - 1] = realMark;
-        }
-        public void SetMark(string className, string studentName, string studentSurname, string lessonName, float mark)
-        {
-            Class schClass = FindClass(className);
-            if (schClass == null)
-            {
-                Console.WriteLine($"Класса с номером {className} не существует");
-                return;
-            }
-
-            Student student = FindStudent(schClass, studentName, studentSurname);
-            if (student == null)
-            {
-                Console.WriteLine($"Студента {studentSurname} {studentName} не существует");
-                return;
-            }
-
-            float realMark = mark > 2 ? mark < 5 ? mark : 5 : 2;
-            student.LessonsMark.TryGetValue(lessonName, out List<float> marks);
-            if (marks == null)
-            {
-                Console.WriteLine($"Предмет {lessonName} в классе {className} не преподаётся");
-            }
-            else
-            {
-                marks.Add(realMark);
-            }
-        }
-        #endregion
-        #region GetRating
-        public void GetClassRating(out List<Class> classes, out List<float> marks)
-        {
-            classes = new List<Class>();
-            marks = new List<float>();
-            if (_classes.Count == 0) return;
-            foreach (Class schClass in _classes)
-            {
-                if (schClass.GetLessons().Count == 0) continue;
-                if (schClass.Students.Count == 0) continue;
-
-                classes.Add(schClass);
-                var averangeMarks = schClass.GetAverageRatingOfClass();
-                float sumMarks = 0;
-                float lessonsCount = 0;
-                foreach (string lesson in averangeMarks.Keys)
-                {
-                    List<float> lessonsMark = averangeMarks[lesson];
-                    if (lessonsMark.Count == 0) continue;
-
-                    lessonsCount++;
-                    sumMarks += lessonsMark.Sum() / lessonsMark.Count();
-                }
-                marks.Add(lessonsCount > 0 ? sumMarks / lessonsCount : 0);
-            }
-
-            //Не бейте за пузырька
-            Class mejClass;
-            float mejMark;
-            for (int i = 0; i < marks.Count; i++)
-            {
-                for (int j = i + 1; j < marks.Count; j++)
-                {
-                    if (marks[i] < marks[j])
-                    {
-                        mejClass = classes[i];
-                        mejMark = marks[i];
-
-                        marks[i] = marks[j];
-                        marks[j] = mejMark;
-                        classes[i] = classes[j];
-                        classes[j] = mejClass;
-                    }
-                }
-            }
-        }
-        #endregion
-
-        public List<float> GetMarks(string className, string studentName, string studentSurname, string lessonName)
-        {
-            Class schClass = FindClass(className);
-            if (schClass == null)
-            {
-                Console.WriteLine($"Класса с номером {className} не существует");
-                return null;
-            }
-            Student student = FindStudent(schClass, studentName, studentSurname);
-            if (student == null)
-            {
-                Console.WriteLine($"Студента {studentSurname} {studentName} не существует");
-                return null;
-            }
-            return student.LessonsMark[lessonName];
-        }
-
-        public List<string> GetLessons(string className)
-        {
-            List<string> lessons = FindClass(className).GetLessons();
-            return lessons;
-        }
-        #endregion
-        #region FindInBase
-        public Class FindClass(string name)
-        {
-            foreach (Class schooleClass in _classes)
-                if (schooleClass.Name.ToLower() == name.ToLower()) return schooleClass;
-            return null;
-        }
-
-        public Teacher FindTeacher(string name, string surname)
-        {
-            if (_teachers.Count > 0)
-                foreach (Teacher teacher in _teachers)
-                    if (teacher.Name.ToLower() == name.ToLower() && teacher.Surname.ToLower() == surname.ToLower()) return teacher;
-            return null;
-        }
-
-        public Student FindStudent(Class schClass, string name, string surname)
-        {
-            foreach (Student student in schClass.Students)
-                if (student.Name.ToLower() == name.ToLower() && student.Surname.ToLower() == surname.ToLower()) return student;
-            return null;
-        }
-        #endregion
-    }
 }
